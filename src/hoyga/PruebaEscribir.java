4844eed5129b37a52b2d853e8c927a1d30e83621
Tomás <ScorpioDominator@gmail.com>
2018-11-16 09:57:56 +0100

    Commit1
    cambio de función

---
+++ src/hoyga/PruebaEscribir.java
@@ -0,0 +1,16 @@
+package hoyga;
+
+public class PruebaEscribir {
+
+	public static void main(String[] args) {
+		
+		int i=1;
+		boolean exit=false;
+		
+		if(i<2) {
+			System.out.println(exit);
+		}
+
+	}
+
+}
